f41db7121863954a77bb72d9b702702f24a98233
Mateus Nicola <mateus.nicola@hotmail.com>
2025-12-27 16:04:59 -0300

    Melhorias no layout e inclusão de novos campos de endereço para o cliente.

--- Models/ClienteModel.cs
+++ Models/ClienteModel.cs
@@ -24,5 +24,14 @@ namespace PhotoStudio.app.Models
         public DateTime CreatedAt { get; set; } = DateTime.Now;
 
         public string UsuarioId { get; set; }
+
+        public string Logradouro { get; set; } = "";
+        public string Numero { get; set; } = "";
+        public string Complemento { get; set; } = "";
+        public string Bairro { get; set; } = "";
+        public string CEP { get; set; } = "";
+        public string Cidade { get; set; } = "";
+        public string Estado { get; set; } = "";
+        public string Pais { get; set; } = "Brasil";
     }
 }
